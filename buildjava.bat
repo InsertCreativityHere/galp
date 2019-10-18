af28e5c054ce83e7672335c28ac50213e255a325
Austin Henriksen <austin.r.henriksen79@gmail.com>
2019-10-18 19:08:33 -0400

    Improved build scripts

--- buildjava.bat
+++ buildjava.bat
@@ -1,10 +1,12 @@
 @ECHO OFF
 
-ECHO Building Galp core...
+ECHO ====> Building Galp core...
 javac -d ./build/ ./src/java/*.java
+IF ERRORLEVEL 1 EXIT
 
-ECHO Building Triggers package...
-javac -d ./build/ ./src/java/trigger/*.java
+ECHO ====> Building Triggers package...
+javac -d ./build/ -cp ./build/ ./src/java/trigger/*.java
+IF ERRORLEVEL 1 EXIT
 
 ECHO Finished.
 ECHO
