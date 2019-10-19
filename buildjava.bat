572f2aea2f23238e96ced79590edc28c5654def4
Austin Henriksen <austin.r.henriksen79@gmail.com>
2019-10-18 21:23:08 -0400

    Updated build scripts to create executable jars

--- buildjava.bat
+++ buildjava.bat
@@ -1,12 +1,25 @@
 @ECHO OFF
 
-ECHO ====> Building Galp core...
-javac -d ./build/ ./src/java/*.java
+ECHO ====> Generating directories...
+MKDIR ./build/classes
+MKDIR ./build/res
 IF ERRORLEVEL 1 EXIT
 
-ECHO ====> Building Triggers package...
-javac -d ./build/ -cp ./build/ ./src/java/trigger/*.java
+ECHO ====> Compiling Galp core...
+javac -d ./build/classes/ ./src/java/*.java
 IF ERRORLEVEL 1 EXIT
 
-ECHO Finished.
+ECHO ====> Compiling Triggers package...
+javac -d ./build/classes/ -cp ./build/classes/ ./src/java/trigger/*.java
+IF ERRORLEVEL 1 EXIT
+
+echo ====> Copying Manifest...
+COPY ./src/java/manifest.mf ./build/res/manifest.mf
+IF ERRORLEVEL 1 EXIT
+
+echo ====> Building jar file...
+jar cfm ./build/galp.jar ./build/res/manifest.mf -C ./build/classes .
+IF ERRORLEVEL 1 EXIT
+
+ECHO Finished!
 ECHO
--- buildjava.sh
+++ buildjava.sh
@@ -1,10 +1,23 @@
-echo ====\> Building Galp core...
-javac -d ./build/ ./src/java/*.java
+echo ====\> Generating directories...
+mkdir ./build/classes
+mkdir ./build/res
 if [ $? -ne 0 ]; then exit; fi
 
-echo ====\> Building Triggers package...
-javac -d ./build/ -cp ./build/ ./src/java/trigger/*.java
+echo ====\> Compiling Galp core...
+javac -d ./build/classes/ ./src/java/*.java
 if [ $? -ne 0 ]; then exit; fi
 
-echo Finished.
+echo ====\> Compiling Triggers package...
+javac -d ./build/classes/ -cp ./build/classes ./src/java/trigger/*.java
+if [ $? -ne 0 ]; then exit; fi
+
+echo ====\> Copying Manifest...
+cp ./src/java-manifest.mf ./build/res/manifest.mf
+if [ $? -ne 0 ]; then exit; fi
+
+echo ====\> Building jar file...
+jar cfmv ./build/galp.jar ./build/res/manifest.mf -C ./build/classes .
+if [ $? -ne 0 ]; then exit; fi
+
+echo Finished!
 echo
