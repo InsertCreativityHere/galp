4639fdc1b59847b336861c8eaf43e43d1ecc6642
Austin Henriksen <austin.r.henriksen79@gmail.com>
2019-10-24 13:46:27 -0400

    Fixed Windows build scripts

--- clean.bat
+++ clean.bat
@@ -1,12 +1,16 @@
-REM TODO I NEED TO TEST THESE BUILD SCRIPTS ON WINDOWS AND ADD VERBOSE MODES WHERE NEEDED!
-
-REM This prevents the commands in this file from being displayed on the command line.
 @ECHO OFF
+REM This prevents the commands in this file from being displayed on the command line.
 
-ECHO     ====> Cleaning build files...
-
-REM Deletes every file and folder inside the 'builds' folder.
-rmdir /s /q "./build/*"
+REM Moves into the 'build' folder
+CD build
+REM Deletes every file inside the 'builds' folder except those ending in '.gitkeep'
+FOR /R %%f IN ("*") DO IF NOT %%~xf==.gitkeep (
+    ECHO Deleting file %%f && DEL /Q %%f
+)
+REM Deletes every folder inside the 'builds' folder.
+FOR /D %%d IN ("*") DO ECHO Deleting folder %%d && RMDIR /S /Q %%d
+REM Moves out of the 'build' folder back into 'galp'
+CD ..
 
 ECHO Finished.
-ECHO
+ECHO.
